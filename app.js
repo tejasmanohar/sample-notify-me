ca30a60af6dd2fe745413319e450fec48850ebae
Tejas Manohar <me@tejasmanohar.com>
2014-09-14 06:08:43 -0500

    fix bugs

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var request = require('request');
 
-function sendReq(body, name) {
+function sendReq(body) {
   request.post(
     'http://buddhabrudda.mybluemix.net/notifications/4699553379', 
     {form:{
       text: body,
-      user: name
+      user: '4699553379'
     }}
   );
 }
 
-sendReq('$15 spent in the last month','Plaid');
-setTimeout(sendReq('Without further ado, your streak will be lost in 5 minutes','Github'), 5000);
-setTimeout(sendReq('There is a new uber driver in your area', 'Uber'), 6000);
-setTimeout(sendReq('The number of cars for sale in your area increased by one','Edmunds'), 9000);
-setTimeout(sendReq('MSFT is declining', 'Bloomberg'), 7500);
-setTimeout(sendReq('AMZN is increasing greatly','USA Today'), 5500);
+setInterval(function () {
+  sendReq('$15 spent in the last month --Plaid');
+  setTimeout(sendReq('Without further ado, your streak will be lost in 5 minutes --Github'), 5000);
+  setTimeout(sendReq('There is a new uber driver in your area --Uber'), 6000);
+  setTimeout(sendReq('AMZN is increasing greatly --USA Today'), 5500);
+  setTimeout(sendReq('The number of cars for sale in your area increased by one --Edmunds'), 9000);
+  setTimeout(sendReq('MSFT is declining --Bloomberg'), 7500);
+  setTimeout(sendReq('$15 spent in the last month --Plaid'), 6000)
+  setTimeout(sendReq('AMZN is increasing greatly --USA Today'), 5500);
+  setTimeout(sendReq('MSFT is declining --Bloomberg'), 7500);
+}, 50000)
